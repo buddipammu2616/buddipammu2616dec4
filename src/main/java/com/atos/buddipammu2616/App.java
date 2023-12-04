78dc366d6d34b67b165cb2f58810ec6e4e38ca0c
mahesh <buddipammu2616@gmail.com>
2023-12-04 23:47:19 +0530

    files

---
+++ src/main/java/com/atos/buddipammu2616/App.java
@@ -0,0 +1,13 @@
+package com.atos.buddipammu2616;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
